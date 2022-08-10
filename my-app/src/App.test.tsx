40c25404dd6291c429c0e27b2f027c795c999028
NurgaliyevS <snurgaliev@cic.kz>
2022-08-10 10:31:38 +0600

    Test upd

--- my-app/src/App.test.tsx
+++ my-app/src/App.test.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders learn react link', async () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  await waitFor(() => {
+    const emailInput = screen.getByText(/content/i);
+    expect(emailInput).toBeInTheDocument();
+  });
 });
